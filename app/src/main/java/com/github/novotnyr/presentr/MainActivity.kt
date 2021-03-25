6964336d25a14f2de8d6c3fb7f0f535602327d2f
Robert Novotny <novotnr0@gmail.com>
2021-03-25 01:37:27 +0100

    Pripraveny prazdny adapter pre ListView

--- app/src/main/java/com/github/novotnyr/presentr/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/presentr/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.github.novotnyr.presentr
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var userListViewAdapter: ArrayAdapter<User>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userListViewAdapter = ArrayAdapter<User>(this, android.R.layout.simple_list_item_1)
+
+        val userListView: ListView = findViewById(R.id.userListView)
+        userListView.adapter = userListViewAdapter
     }
 
     fun onFloatingActionButtonClick(view: View) {}
