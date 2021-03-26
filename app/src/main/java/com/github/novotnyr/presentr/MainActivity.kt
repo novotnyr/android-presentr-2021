e4fa98c903e3e7ad6a4d7f95a078495d20f16f41
Robert Novotny <novotnr0@gmail.com>
2021-03-26 10:34:57 +0100

    Pridana obsluha tlacidla na liste app bar

--- app/src/main/java/com/github/novotnyr/presentr/MainActivity.kt
+++ app/src/main/java/com/github/novotnyr/presentr/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.github.novotnyr.presentr
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
@@ -33,4 +35,19 @@ class MainActivity : AppCompatActivity() {
         Snackbar.make(view, "Používateľ je v miestnosti!", Snackbar.LENGTH_SHORT)
             .show()
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        when (item.itemId) {
+            R.id.menu_item_refresh -> {
+                userViewModel.refresh()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+
 }
